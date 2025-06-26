46d92606d9d132ae2fcf7c660091df225fad47fc
hamzadaqa <abudaqahamza@gmail.com>
2025-06-26 18:37:59 -0400

    removed api key and put in .env file

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,20 +1,19 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyD02oG2idnkSBXd6ZfGrtDNZ9-OX05PVs4",
-  authDomain: "coverly-884ca.firebaseapp.com",
-  projectId: "coverly-884ca",
-  storageBucket: "coverly-884ca.firebasestorage.app",
-  messagingSenderId: "400810257269",
-  appId: "1:400810257269:web:4e10d071d573f4602a413c",
-  measurementId: "G-FQ94YGCH8F"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-console.log("Logged in user:", auth.currentUser);
 const googleProvider = new GoogleAuthProvider();
 
 export { auth, googleProvider };

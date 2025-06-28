fb7bf5ded437bb8f3317ca63c77586b9e04d15ae
hamzadaqa <abudaqahamza@gmail.com>
2025-06-28 15:39:57 -0400

    firebase setup

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  authDomain: "coverly-884ca.firebaseapp.com",
+  projectId: "coverly-884ca",
+  storageBucket: "coverly-884ca.appspot.com",
+  messagingSenderId: "400810257269",
+  appId: "1:400810257269:web:4e10d071d573f4602a413c",
+  measurementId: "G-FQ94YGCH8F"
 };
 
 const app = initializeApp(firebaseConfig);
